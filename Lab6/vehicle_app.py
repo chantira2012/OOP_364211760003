7fc5986a04aedf208e8bd635a3229cea1baf623a
chantira <chantira.p@rmutsvmail.com>
2022-09-07 17:54:12 +0700

    Lab6 exam

--- Lab6/vehicle_app.py
+++ Lab6/vehicle_app.py
@@ -1,8 +1,21 @@
 from vehicle import vehicle
+
+# display
+def display_vehicle():
+    if len(my_vehicle) == 0:
+            print("you had no vehicle data.")
+    else:
+        print(f'you had {len(my_vehicle)} following:')
+        for x in my_vehicle:
+            x.vehicle_detail()
+            print("\n")
+
+
+
 #option
-def display_option
+def display_option():
     select = 0
-    print("welcom to vehicle data store system (VDSS)")
+    print("welcome to vehicle data store system (VDSS)")
     print("1.add vehicle")
     print("2.display all vehicle")
     print("3.exit")
@@ -10,10 +23,11 @@ def display_option
     if select == 1:
         input_vehicle_data()
     elif select == 2:
-        pass
+        display_vehicle()
     elif select == 3:
+        print("Good Bye")
         exit(0)
-    elif:
+    else:
         print("please, select number 1-3.")
 
 #input data
@@ -25,9 +39,15 @@ def input_vehicle_data():
     price = float(input("Enter vehicle price: "))
 
     #return breadname,model,color,max_speen,price #return as tuple
-    return brandname,model,color,max_speed,price #return as tuple
+    my_vehicle.append(vehicle(brandname,model,color,max_speed,price)) #return as tuple
+    print("\n-------------------------------------------------")
+    print("already add vehicle to store.")
+    print("\n-------------------------------------------------")
 
 my_vehicle = []
+s = 0
+while s == 0:
+    display_option()
 
 
 #vm=input_vehicle_data()
